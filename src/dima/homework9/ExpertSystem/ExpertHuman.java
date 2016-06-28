dd629e3d076c6457e4f16bc1de427f905f53355f
dmitriy <dmitriy.vladimirovich@gmail.com>
2016-06-28 17:50:00 +0300

    добавил еще домашек с ноута

--- src/dima/homework9/ExpertSystem/ExpertHuman.java
+++ src/dima/homework9/ExpertSystem/ExpertHuman.java
@@ -3,9 +3,17 @@ package dima.homework9.ExpertSystem;
 /**
  * Created by dmitriy on 28.06.16.
  */
-public class ExpertHuman {
+public abstract class ExpertHuman {
     protected static String firstName;
     protected static String lastName;
+
+
+    abstract protected void setFirstName();
+    abstract protected void getFirstName();
+    abstract protected void setLastName();
+    abstract protected void getLastName();
+
+
 
 
 }
---
+++ src/dima/homework9/ExpertSystem/ExpertTeacher.java
@@ -0,0 +1,26 @@
+package dima.homework9.ExpertSystem;
+
+/**
+ * Created by dmitriy on 28.06.16.
+ */
+public class ExpertTeacher extends ExpertHuman {
+    @Override
+    protected void setFirstName() {
+
+    }
+
+    @Override
+    protected void getFirstName() {
+
+    }
+
+    @Override
+    protected void setLastName() {
+
+    }
+
+    @Override
+    protected void getLastName() {
+
+    }
+}
